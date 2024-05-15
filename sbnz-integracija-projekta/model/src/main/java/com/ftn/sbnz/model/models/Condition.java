3b90d67baa2354e49e45e84d3418d3c55e978b02
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2024-05-15 15:38:31 +0200

    Change Condition Type to enum

--- sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/Condition.java
+++ sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/Condition.java
@@ -3,30 +3,42 @@ package com.ftn.sbnz.model.models;
 import java.util.Objects;
 
 public class Condition {
-    private String type;
+    private ConditionType type;
 
     public Condition() {
-        this.type = "";
+        // Optionally set a default type, or leave it uninitialized (null)
+        this.type = ConditionType.A; // Example default
     }
 
-    public Condition(String type) {
+    public Condition(ConditionType type) {
         this.type = type;
     }
 
-    public String getType() {
+    public ConditionType getType() {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(ConditionType type) {
         this.type = type;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Condition condition = (Condition) o;
+        return type == condition.type;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
 
     @Override
     public String toString() {
         return "Condition{" +
-                "type='" + type + '\'' +
+                "type=" + type +
                 '}';
     }
 }
---
+++ sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/ConditionType.java
@@ -0,0 +1,7 @@
+package com.ftn.sbnz.model.models;
+
+public enum ConditionType {
+    A, B, C, D, E, F, G, H, I, J,
+    K, L, M, N, O, P, Q, R, S, T,
+    U, V, W, X, Y, Z
+}
