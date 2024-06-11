6bd1b138953b062b915259864d04990802c62caf
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2024-06-11 11:56:45 +0200

    Add authencitation

---
+++ sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.ftn.sbnz.model.models.enums;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getValue() {
+        return role;
+    }
+}
